c0be790c4a7f4bcc9160302efd8ede33df98f1d4
camPHE <cameron@rockefeller.edu>
2014-11-09 16:37:50 -0500

    -Cleared up syntax errors. -Modified function calls to loglet() as per discussion -Added print statements in various locations. (uncomment stop() calls to halt the program at these locations).

--- js2R/create_curves.10062014-new.R
+++ js2R/create_curves.10062014-new.R
@@ -25,8 +25,13 @@ b0 <- logobj.parameters.b0
 i <- 0
 j <- 0
 
+##### SETTING d as 0 (almost all the time?) - C.C. 11/09/2014 #####
+d = 0
+
+##### WHAT IS sl ? - C.C. 11/09/2014 ######
 sl = 1.2
 
+##### HARDCODING SUNFLOWER DATA FOR NOW - C.C. 11/09/2014 ######
 logobj.data.x = c(7.0, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84)
 logobj.data.y = c(17.93, 36.36, 67.76, 98.1, 131, 169.5, 205.5, 228.3, 247.1, 250.5, 253.8, 254.5)
 
@@ -35,11 +40,12 @@ xstart = logobj.parameters.b0 - sl * logobj.parameters.a0
 ########## IS THIS DETERMINED BY THE USER??
 logobj.number_of_loglets = 1 
 
+##### RIGHT NOW logobj.parameters IS AN ARBITRARY VALUE - C.C. 11/09/2014 ######
 ####logobj.parameters = 0 
 logobj.parameters = 5
 ####logobj.parameters <- c()
 
-xstop =  logobj.parameters[b+(logobj.number_of_loglets-1)] + sl * logobj.parameters[a+(logobj.number_of_loglets -1)]
+xstop =  logobj.parameters[b0+(logobj.number_of_loglets-1)] + sl * logobj.parameters[a0+(logobj.number_of_loglets -1)]
 
 logobj.curve.n = 6 
 
@@ -65,6 +71,9 @@ logobj.curve.n = 6
     }
   i = i + 1
   }
+##### PRINTING logobj.curve.yfit ########
+print(logobj.curve.yfit)
+####stop()
 
 
 #####logobj.data.x = 0
@@ -81,20 +90,21 @@ logobj.data.x.length = length(logobj.data.x)
     i = i + 1
   }
 
+##### MODIFIED AS PER DISCUSSION - C.C. 11/09/2014 ###### 
+
   while (i < logobj.data.x.length){
     while (j < logobj.number_of_loglets) {
       logobj.data.yfit[i] = logobj.data.yfit[i] + 
-        loglet(logobj.data.x[i],
-               logobj.parameters[a+j],
-               logobj.parameters[k+j],
-               logobj.parameters[b+j]);
-      logobj.data.yfit[i] = logobj.data.yfit[i] + logobj.parameters[d] 
+        loglet(logobj.data.x[i], a0,k0,b0)
       j = j + 1 
     }
     logobj.data.residuals[i] = logobj.data.y[i] - logobj.data.yfit[i]
     i = i + 1
   }
 
+##### PRINTING logobj.data.residuals ######
+print(logobj.data.residuals)
+#####stop()
 
 ##########HOW ABOUT THESE ##############
   mean = 0.0;
@@ -127,6 +137,9 @@ logobj.data.x.length = length(logobj.data.x)
     iter = iter + 1
   }
 
+##### PRINTING logobj.data.sr ######
+print(logobj.data.sr)
+###stop()
 
   ##### put in flot form
   iter = 0
@@ -167,71 +180,86 @@ logobj.data.x.length = length(logobj.data.x)
 
   j = 0
   while (j < logobj.number_of_loglets) {
-    fp[a+j] = logobj.parameters[a+j]
-    ####print(fp[a+j])
-    fp[k+j] = logobj.parameters[k+j]
-    fp[b+j] = logobj.parameters[b+j]
+    fp[a0] = a0
+    fp[k0] = k0
+    fp[b0] = b0
     j = j + 1
   }
-#######stop()
+##### PRINTING fp ######
+print(fp)
+###stop()
 
   ##### makes the fisher pry plots look right ##### 
   j = 0
   while (j < logobj.number_of_loglets) {
-    if ( (fp[a+j] > 0.0) && (fp[k+j] < 0.0) ) {
-      fp[a+j] = -1.0 * fp[a+j]
-      fp[k+j] = -1.0 * fp[k+j]
+    if ( (fp[a0] > 0.0) && (fp[k0] < 0.0) ) {
+      fp[a0] = -1.0 * fp[a0]
+      fp[k0] = -1.0 * fp[k0]
     }
-    if (fp[a+j] < 0.0 && fp[k+j] < 0.0 ) {
-      fp[a+j] = -1.0 * fp[a+j]
-      fp[k+j] = -1.0 * fp[k+j]
+    if (fp[a0] < 0.0 && fp[k0] < 0.0 ) {
+      fp[a0] = -1.0 * fp[a0]
+      fp[k0] = -1.0 * fp[k0]
     }    
   j = j + 1
   }  
 
+##### PRINTING fp again ######
+print(fp)
+####stop()
+
   ##### lets make the fisher pry transformed data ###### 
   ##### and now compoent logisitcs (called cc for some reason #####
 
-  j = 0
-  while (j < logobj.number_of_loglets) {
-    fp[x+j] = c()
-    fp[y+j] = c()
-    cc[x+j] = c()
-    cc[y+j] = c()
+##### HAD TO SET THESE AS SOMETHING... - C.C. 11/09/2014 ######
+    x0 = 0
+    y0 = 0
+    x0 = 0
+    y0 = 0
 
-    i = 0
+  j = 0
+
+##### INTERESTING TO NOTE: R STARTS INDEXING ITS ARRAYS AT 1 AND NOT 0.  THIS WILL LIKELY CHANGE THE INITIAL ITERATION VALUE ON SOME VARIABLES - C.C. 11/09/2014 ######
+
+  while (j < logobj.number_of_loglets) {
+
+    i = 1  
     while (i < logobj.data.x.length) {
-      if ( (logobj.data.x[i] > (logobj.parameters[b+j] - abs(logobj.parameters[a+j] * sl)))
-           &&
-           (logobj.data.x[i] < (logobj.parameters[b+j] + Math.abs(logobj.parameters[a+j] * sl))) ) {
+      if ( (logobj.data.x[i] > (b0 - abs(a0 * sl))) && (logobj.data.x[i] < (b0 + abs(a0 * sl))) ) {
 
         #### THIS LINE IS A PROBLEM #### fp[x+j].push(logobj.data.x[i]) 
         #### THIS LINE IS A PROBLEM #### cc[x+j].push(logobj.data.x[i]) 
         #### THIS LINE IS A PROBLEM #### fp[y+j].push(logobj.data.y[i] - logobj.parameters[d] ) 
         #### THIS LINE IS A PROBLEM #### cc[y+j].push(logobj.data.y[i] - logobj.parameters[d] ) 
         ########## TRYING THIS AS A SOLUTION ##############
-	fp[x+j] <- union(fp[x+j], c(logobj.data.x[i]))
-        cc[x+j] <- union(cc[x+j], c(logobj.data.x[i]))
-        fp[y+j] <- union(fp[y+j], c(logobj.data.y[i] - logobj.parameters[d]))
-        cc[y+j] <- union(cc[y+j], c(logobj.data.y[i] - logobj.parameters[d]))
+	fp[x0] <- union(fp[x0], c(logobj.data.x[i]))
+        cc[x0] <- union(cc[x0], c(logobj.data.x[i]))
+        fp[y0] <- union(fp[y0], c(logobj.data.y[i] - logobj.parameters[d]))
+        cc[y0] <- union(cc[y0], c(logobj.data.y[i] - logobj.parameters[d]))
       }
     i = i + 1
     }
   j = j + 1
   }
+
+##### PRINTING fp and cc######
+print(fp)
+print(cc)
+###stop()
 
   j = 0
   while (j < logobj.number_of_loglets) {
     i = 0
     ###### while (i < cc[x+j].length) {
-    while (i < length(cc[x+j])) {
+    while (i < length(cc[x0])) {
       iter = 0
       while (iter < logobj.number_of_loglets) {
         if ( iter != j) {
-          cc[y+j][i] = cc[y+j][i] - loglet(cc[x+j][i],
-                                 logobj.parameters[a+iter],
-                                 logobj.parameters[k+iter],
-                                 logobj.parameters[b+iter])
+	  ##### THIS LINE DIDN'T ACTUALLY GIVE A SYNTAX ERROR, BUT STILL MODIFIED AS PER DISCUSSION - C.C. 11/09/2014 ######
+          #####	cc[y0][i] = cc[y0][i] - loglet(cc[x0][i],
+          #####                       logobj.parameters[a+iter],
+          #####                       logobj.parameters[k+iter],
+          #####                       logobj.parameters[b+iter])
+          cc[y0][i] = cc[y0][i] - loglet(cc[x0][i], a0, k0, b0)
         }
       iter = iter + 1
       }
@@ -244,37 +272,52 @@ logobj.data.x.length = length(logobj.data.x)
   j = 0
   while (j < logobj.number_of_loglets) {
     i = 0
-    ######while (i < fp[x+j].length) {
-    while (i < length(fp[x+j])) {
+    ######while (i < fp[x0].length) {
+    while (i < length(fp[x0])) {
       iter = 0
       while (iter < logobj.number_of_loglets) {
         if ( iter != j) {
-          fp[y+j][i] = fp[y+j][i] - loglet(fp[x+j][i],
-                                 logobj.parameters[a+iter],
-                                 logobj.parameters[k+iter],
-                                 logobj.parameters[b+iter]);
+##### THIS LINE DIDN'T ACTUALLY GIVE A SYNTAX ERROR, BUT STILL MODIFIED AS PER DISCUSSION - C.C. 11/09/2014 #######
+          #####fp[y0][i] = fp[y0][i] - loglet(fp[x0][i],
+          #####                       logobj.parameters[a+iter],
+          #####                       logobj.parameters[k+iter],
+          #####                       logobj.parameters[b+iter]);
+
+          fp[y0][i] = fp[y0][i] - loglet(fp[x0][i], a0, k0, b0)
         }
       iter = iter + 1
       }
 
-      if (logobj.parameters[k+j] < 0.0) { 
-	fp[y+j][i] = fp[y+j][i] + (-1.0) * logobj.parameters[k+j]
+      if (logobj.parameters[k0] < 0.0) { 
+	fp[y0][i] = fp[y0][i] + (-1.0) * logobj.parameters[k0]
       }
 
-      if (fp[y+j][i] < 0.0 || fp[y+j][i] >= abs(logobj.parameters[k+j])) { 
-        fp[y+j][i] = 0.0
+      if (fp[y0][i] < 0.0 || fp[y0j][i] >= abs(logobj.parameters[k0])) { 
+        fp[y0][i] = 0.0
       } else {
-        fp[y+j][i] = fp[y+j][i] / ( abs( logobj.parameters[k+j]) - fp[y+j][i])
+        fp[y0][i] = fp[y0][i] / ( abs( logobj.parameters[k0]) - fp[y0][i])
       }
     i = i + 1
     }
   j = j + 1
   }  
 
+##### PRINTING fp and cc again ######
+print(fp)
+print(cc)
+###stop()
+
+##### IMPROVISED, FOR NOW... C.C. - 11/9/14 #####
+logobj.fp <- c()
+datax = 0
+datay = 0
+logobj.cc <- c()
+x = 0
+
   j = 0
   while (j < logobj.number_of_loglets) {
-    logobj.flot[fpdata+j] = c()
-    logobj.flot[ccdata+j] = c()
+    ####logobj.flot[fpdata+j] = c()
+    ####logobj.flot[ccdata+j] = c()
     logobj.fp[datax + j] = c()
     logobj.fp[datay + j] = c()
     logobj.cc[datax + j] = c()
@@ -297,31 +340,42 @@ logobj.data.x.length = length(logobj.data.x)
 
   ###### now lets make the fisher py lines ######### 
 
+##### IMPROVISED, FOR NOW... C.C. - 11/9/14 #####
+xl0 = 0
+yl0 = 0
+linex = 0
+liney = 0
+
   j = 0
   while (j < logobj.number_of_loglets) {
-    fp[xl+j] = linspace(xstart,xstop,100) ##### this looks like bug? xstart should be changing.. 
-    fp[yl+j] = c()
+    fp[xl0] = linspace(xstart,xstop,100) ##### this looks like bug? xstart should be changing.. 
+    fp[yl0] = c()
     
     i = 0
     #####while (i < fp[xl+j].length) {
-    while (i < length(fp[xl+j])) {
-      fp[yl+j][i] = loglet(fp[xl+j][i], fp[a+j], fp[k+j], fp[b+j])
-      fp[yl+j][i] = fp[yl+j][i] / fp[k+j]  ###### create fraction f (y/k) #####
-      fp[yl+j][i] = fp[yl+j][i] / ( 1 - fp[yl+j][i]) #### create f / (1 - f) ##### 
+    while (i < length(fp[xl0])) {
+      fp[yl0][i] = loglet(fp[xl0][i], fp[a0], fp[k0], fp[b0])
+      fp[yl0][i] = fp[yl0][i] / fp[k0]  ###### create fraction f (y/k) #####
+      fp[yl0][i] = fp[yl0][i] / ( 1 - fp[yl0][i]) #### create f / (1 - f) ##### 
     i = i + 1
     }
   j = j + 1
   }
 
+##### PRINTING fp  again ######
+print(fp)
+###stop()
+
+
   j = 0
   while (j < logobj.number_of_loglets) {
-    logobj.flot[fpline+j] = c()
+    ####logobj.flot[fpline+j] = c()
     logobj.fp[linex + j] = c()
     logobj.fp[liney + j] = c()
 
     i = 0
     #####while (i < fp[xl+j].length) {
-    while (i < length(fp[xl+j])) {
+    while (i < length(fp[xl0])) {
       #### THIS LINE IS A PROBLEM logobj.flot[fpline+j].push([fp[xl+j][i],log10(fp[yl+j][i])])
       #### THIS LINE IS A PROBLEM logobj.fp[linex+j].push(fp[xl+j][i])
       #### THIS LINE IS A PROBLEM logobj.fp[liney+j].push(fp[yl+j][i])
@@ -330,33 +384,44 @@ logobj.data.x.length = length(logobj.data.x)
   j = j + 1
   }
 
+##### PRINTING logobj.fp ######
+print(logobj.fp)
+#####stop()
 
   ##### now lets make component curves ######### 
 
   j = 0
   while (j < logobj.number_of_loglets) {
-    xstart = logobj.parameters[b+j] - sl * logobj.parameters[a+j]
-    xstop =  logobj.parameters[b+j] + sl * logobj.parameters[a+j]
-    cc[xl+j] = linspace(xstart,xstop,100) ###### this looks like bug? xstart should be changing.. 
-    cc[yl+j] = c()
+    xstart = logobj.parameters[b0] - sl * logobj.parameters[a0]
+    xstop =  logobj.parameters[b0] + sl * logobj.parameters[a0]
+    cc[xl0] = linspace(xstart,xstop,100) ###### this looks like bug? xstart should be changing.. 
+    cc[yl0] = c()
 
     i = 0
     ####while (i < cc[xl+j].length) {
-    while (i < length(cc[xl+j])) {
-      cc[yl+j][i] = loglet(cc[xl+j][i],fp[a+j],fp[k+j], fp[b+j])
+    while (i < length(cc[xl0])) {
+      cc[yl0][i] = loglet(cc[xl0][i],fp[a0],fp[k0], fp[b0])
       i = i + 1
     }
   j = j + 1
   }
 
+##### PRINTING cc ######
+print(cc)
+####stop()
+
   j = 0
   while (j < logobj.number_of_loglets) {
-    logobj.flot[cccurve+j] = c()
+    ########logobj.flot[cccurve+j] = c()
     logobj.cc[linex + j] = c()
     logobj.cc[liney + j] = c()
 
+##### PRINTING logobj.cc ######
+print(logobj.cc)
+###stop()
+
     i = 0
-    while (i < length(cc[xl+j])) {
+    while (i < length(cc[xl0])) {
       #### THIS LINE IS A PROBLEM logobj.flot[cccurve+j].push([cc[xl+j][i],cc[yl+j][i]])
       #### THIS LINE IS A PROBLEM logobj.cc[linex+j].push(cc[xl+j][i])
       #### THIS LINE IS A PROBLEM logobj.cc[liney+j].push(cc[yl+j][i])
@@ -368,4 +433,3 @@ logobj.data.x.length = length(logobj.data.x)
     }
   j = j + 1
   }
-
